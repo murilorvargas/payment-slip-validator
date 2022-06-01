e534928e27cb14d39c1fa607653559cf29c4d11b
Murilo Vargas <muriraubach@gmail.com>
2022-06-01 19:24:31 -0300

    feat: add bar code is valid in payment slip provider interface

--- src/shared/container/providers/PaymentSlipProvider/IPaymentSlipProvider.ts
+++ src/shared/container/providers/PaymentSlipProvider/IPaymentSlipProvider.ts
@@ -1,6 +1,7 @@
 interface IPaymentSlipProvider {
   fieldIsValid(field: string): boolean;
   getBarCode(payment_slip: string): string;
+  barCodeIsValid(bar_code: string): boolean;
   getAmount(payment_slip: string): string;
   getExpirationDate(expirationFactor: string): Date;
 }
