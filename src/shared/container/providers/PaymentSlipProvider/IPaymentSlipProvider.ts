029a69726541e160faa81fcffcb856647f9e0216
Murilo Vargas <muriraubach@gmail.com>
2022-06-01 18:52:51 -0300

    feat: add get amount method in payment slip provider interface

--- src/shared/container/providers/PaymentSlipProvider/IPaymentSlipProvider.ts
+++ src/shared/container/providers/PaymentSlipProvider/IPaymentSlipProvider.ts
@@ -1,6 +1,7 @@
 interface IPaymentSlipProvider {
   fieldIsValid(field: string): boolean;
   getBarCode(payment_slip: string): string;
+  getAmount(payment_slip: string): string;
   getExpirationDate(expirationFactor: string): Date;
 }
 
